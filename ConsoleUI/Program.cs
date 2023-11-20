f04d4ea58d8abe0dd66bc9db77bf47c5e8861930
iloozealot <iloozealot@gmail.com>
2023-11-20 17:52:56 -0600

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using DemoLibrary.Models;
+
+PersonModel person = new PersonModel();
+
+person.LastName = "Spencer";
+person.FirstName = "Justin";
+
+Console.WriteLine($"Hello {person.FirstName} {person.LastName}");
+
+Console.ReadLine();
